aa5e42d3a7feb5b2f1a26edab3c953d9f863c508
Mirae <mirae2thefuture@gmail.com>
2024-08-08 17:20:46 +0900

    [LTM-21] feat: ViewTemplate 추가 및 파일 계층 수정

---
+++ LottoMate/LottoMate/Views/Common/ViewTemplate.swift
@@ -0,0 +1,31 @@
+//
+//  LotteryTypeButtonsView.swift
+//  LottoMate
+//
+//  Created by Mirae on 8/8/24.
+//
+
+import UIKit
+import PinLayout
+import FlexLayout
+
+class ViewTemplate: UIView {
+    fileprivate let rootFlexContainer = UIView()
+    
+    init() {
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+}
+
+#Preview {
+    let view = ViewTemplate()
+    return view
+}
