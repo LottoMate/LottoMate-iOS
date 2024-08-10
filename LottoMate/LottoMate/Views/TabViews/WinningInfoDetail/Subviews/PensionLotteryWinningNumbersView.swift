c5f9bcf450bcc3a87d32938b728fd46f331905a6
Mirae <mirae2thefuture@gmail.com>
2024-08-10 22:10:11 +0900

    [LTM-21] feat: PensionLotteryWinningNumbersView 작업

--- LottoMate/LottoMate/Views/TabViews/WinningInfoDetail/Subviews/PensionLotteryWinningNumbersView.swift
+++ LottoMate/LottoMate/Views/TabViews/WinningInfoDetail/Subviews/PensionLotteryWinningNumbersView.swift
@@ -12,12 +12,21 @@ import FlexLayout
 class PensionLotteryWinningNumbersView: UIView {
     fileprivate let rootFlexContainer = UIView()
     
+    let groupAndNumbersContainer = UIView()
+    
     let rankLabel = UILabel()
-    let winningNumbersContainer = UIView()
     let groupContainer = UIView()
     let groupNumberBall = WinningNumberCircleView()
     var groupNumber: Int?
     let groupLabel = UILabel()
+    
+    let winningNumbersContainer = UIView()
+    let firstPensionLotteryNumber = WinningNumberCircleView()
+    let secondPensionLotteryNumber = WinningNumberCircleView()
+    let thirdPensionLotteryNumber = WinningNumberCircleView()
+    let fourthPensionLotteryNumber = WinningNumberCircleView()
+    let fifthPensionLotteryNumber = WinningNumberCircleView()
+    let sixthPensionLotteryNumber = WinningNumberCircleView()
     
     init(groupNumber: Int) {
         super.init(frame: .zero)
@@ -33,13 +42,40 @@ class PensionLotteryWinningNumbersView: UIView {
         
         groupNumberBall.number = 4
         groupNumberBall.circleColor = .black
+        
+        firstPensionLotteryNumber.number = 8
+        firstPensionLotteryNumber.circleColor = .ltmRed
+        secondPensionLotteryNumber.number = 1
+        secondPensionLotteryNumber.circleColor = .ltmPeach
+        thirdPensionLotteryNumber.number = 7
+        thirdPensionLotteryNumber.circleColor = .ltmYellow
+        fourthPensionLotteryNumber.number = 5
+        fourthPensionLotteryNumber.circleColor = .ltmGreen
+        fifthPensionLotteryNumber.number = 1
+        fifthPensionLotteryNumber.circleColor = .ltmBlue
+        sixthPensionLotteryNumber.number = 9
+        sixthPensionLotteryNumber.circleColor = .ltmRed
 
         rootFlexContainer.flex.direction(.column).paddingVertical(24).paddingHorizontal(20).define { flex in
             flex.addItem(rankLabel).alignSelf(.start).marginBottom(8)
-            flex.addItem(groupContainer).direction(.row).define { flex in
-                flex.addItem(groupNumberBall).width(30).height(30).marginRight(8)
-                flex.addItem(groupLabel)
+            
+            flex.addItem(groupAndNumbersContainer).direction(.row).define { flex in
+                flex.addItem(groupContainer).direction(.row).paddingRight(24).define { flex in
+                    flex.addItem(groupNumberBall).width(30).height(30).marginRight(8)
+                    flex.addItem(groupLabel)
+                }
+                flex.addItem(winningNumbersContainer).direction(.row).justifyContent(.spaceBetween).define { flex in
+                    flex.addItem(firstPensionLotteryNumber).width(30).height(30)
+                    flex.addItem(secondPensionLotteryNumber).width(30).height(30)
+                    flex.addItem(thirdPensionLotteryNumber).width(30).height(30)
+                    flex.addItem(fourthPensionLotteryNumber).width(30).height(30)
+                    flex.addItem(fifthPensionLotteryNumber).width(30).height(30)
+                    flex.addItem(sixthPensionLotteryNumber).width(30).height(30)
+                }
+                .grow(1)
             }
+            
+            flex.addItem().height(1).marginTop(16).backgroundColor(.lightGray) // 컬러 변경 필요 
         }
         
         addSubview(rootFlexContainer)
