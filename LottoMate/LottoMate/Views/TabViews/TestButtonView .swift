7efd79cb1f912f250f0f4d6811c097323319c990
Mirae <mirae2thefuture@gmail.com>
2024-08-20 17:44:22 +0900

    [LTM-22] [ FEAT ] add HTML parsing library and test with sample HTML text

--- LottoMate/LottoMate/Views/TabViews/TestButtonView .swift
+++ LottoMate/LottoMate/Views/TabViews/TestButtonView .swift
@@ -8,12 +8,12 @@
 import UIKit
 import FlexLayout
 import PinLayout
+import SwiftSoup
 
 class TestButtonView: UIView {
     fileprivate let rootFlexContainer = UIView()
     public let defaultSolidButton = StyledButton(title: "Test Button", buttonStyle: .solid(.large, .active), fontSize: 16, cornerRadius: 8, verticalPadding: 0, horizontalPadding: 0)
-    let testLabel = UILabel()
-    let testLabel2 = UILabel()
+    let textView = UITextView()
     
     init() {
         super.init(frame: .zero)
@@ -21,18 +21,23 @@ class TestButtonView: UIView {
         
         defaultSolidButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
         
-        testLabel.text = "당첨 번호 상세"
-        styleLabel(for: testLabel, fontStyle: .caption, textColor: .black)
-        testLabel2.text = "당첨 번호 정보"
-        styleLabel(for: testLabel2, fontStyle: .headline1, textColor: .black)
-
-             
+        let boldTextAttributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.boldSystemFont(ofSize: 16) // 굵은 폰트
+        ]
+        
+        let smallTextAttributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.systemFont(ofSize: 12) // 작은 폰트
+        ]
+        
+        
+//        textView.font = UIFont.systemFont(ofSize: 14)
+        parseHtml()
+        
         addSubview(rootFlexContainer)
         
-        rootFlexContainer.flex.direction(.column).alignItems(.center).define { flex in
-            flex.addItem(defaultSolidButton).width(127).height(48)
-            flex.addItem(testLabel).border(1, .blue)
-            flex.addItem(testLabel2).border(1, .red)
+        rootFlexContainer.flex.direction(.column).alignItems(.center).paddingHorizontal(10).define { flex in
+            flex.addItem(defaultSolidButton).width(127).height(48).marginBottom(48)
+            flex.addItem(textView).grow(1)
         }
     }
     
@@ -51,24 +56,49 @@ class TestButtonView: UIView {
         print("Button tapped")
     }
     
-    func setLineHeight(for label: UILabel, lineHeight: CGFloat) {
-        // 텍스트가 없으면 빈 문자열로 초기화
-        let text = label.text ?? ""
-        
-        // NSMutableParagraphStyle을 생성하고 line height를 설정
-        let paragraphStyle = NSMutableParagraphStyle()
-        paragraphStyle.minimumLineHeight = lineHeight
-        paragraphStyle.maximumLineHeight = lineHeight
-        paragraphStyle.alignment = .left // 원하는 정렬 방식으로 설정
-        
-        // NSAttributedString을 생성하고 텍스트와 paragraphStyle을 적용
-        let attributes: [NSAttributedString.Key: Any] = [
-            .paragraphStyle: paragraphStyle
-        ]
-        let attributedString = NSAttributedString(string: text, attributes: attributes)
-        
-        // UILabel에 attributedText를 설정
-        label.attributedText = attributedString
+    func parseHtml() {
+        do {
+            let html = SampleHtmlDoc.sampleData
+            let doc: Document = try SwiftSoup.parse(html)
+            let elements: Elements = try doc.select("div")
+            
+            let lineSpacing: CGFloat = 2.0
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.lineSpacing = lineSpacing
+            
+            let attributedString = NSMutableAttributedString()
+            
+            for div in elements {
+                let text = try div.text()
+                var attributes = [NSAttributedString.Key: Any]()
+                
+                if text.first == "▶" {
+                    let boldTextAttributes: [NSAttributedString.Key: Any] = [
+                        .font: UIFont.boldSystemFont(ofSize: 14),
+                        .paragraphStyle: paragraphStyle
+                    ]
+                    let atrributedText = NSAttributedString(string: (text + "\n"), attributes: boldTextAttributes)
+                    attributedString.append(atrributedText)
+                    
+                } else if text.prefix(2) == "->" {
+                    let smallTextAttributes: [NSAttributedString.Key: Any] = [
+                        .font: UIFont.systemFont(ofSize: 12),
+                        .paragraphStyle: paragraphStyle
+                    ]
+                    let atrributedText = NSAttributedString(string: (text + "\n\n"), attributes: smallTextAttributes)
+                    attributedString.append(atrributedText)
+                    
+                } else {
+                    // Default style 주기
+                }
+            }
+            textView.attributedText = attributedString
+            
+        } catch Exception.Error(_, let message) {
+            print("parseHtml message: \(message)")
+        } catch {
+            print("An error occurs parsing html doc.")
+        }
     }
 }
 
