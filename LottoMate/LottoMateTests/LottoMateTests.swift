6a0d8b5b69c69353f313516c15a4e9c3c7400520
Mirae <mirae2thefuture@gmail.com>
2024-10-18 01:33:33 +0900

    [LTM-87] [ FEAT ] Add test for marker update in MapViewController
    현재 위치 마커 업데이트 기능을 테스트합니다.
    다음 사항들을 검증:
    - 초기 마커가 nil인지 확인
    - 첫 위치 업데이트로 마커가 생성되는지 확인
    - 이후 위치 변경 시 마커가 정확히 업데이트되는지 확인
    - 이전 마커들이 적절히 제거되는지 확인

--- LottoMate/LottoMateTests/LottoMateTests.swift
+++ LottoMate/LottoMateTests/LottoMateTests.swift
@@ -6,31 +6,65 @@
 //
 
 import XCTest
+import RxSwift
+import RxTest
+import RxBlocking
+import CoreLocation
+import NMapsMap
 @testable import LottoMate
 
 final class LottoMateTests: XCTestCase {
+    var viewController: MapViewController?
+    var reactor: MapViewReactor?
+    var disposeBag: DisposeBag?
+    var scheduler: TestScheduler?
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        viewController = MapViewController()
+        reactor = MapViewReactor()
+        viewController?.reactor = reactor
+        disposeBag = DisposeBag()
+        scheduler = TestScheduler(initialClock: 0)
+        
+        // Load the view hierarchy
+        viewController?.loadViewIfNeeded()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        viewController = nil
+        reactor = nil
+        disposeBag = nil
+        scheduler = nil
+        try super.tearDownWithError()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testUpdateMarker() throws {
+        guard let viewController = viewController else {
+            XCTFail("ViewController is nil")
+            return
         }
+        
+        // 서울시청
+        let location1 = CLLocation(latitude: 37.550263, longitude: 126.9970831)
+        // N서울타워
+        let location2 = CLLocation(latitude: 37.5511694, longitude: 126.9882266)
+        
+        XCTAssertNil(viewController.currentMarker)
+        
+        viewController.updateMarker(at: location1)
+        
+        XCTAssertNotNil(viewController.currentMarker)
+        XCTAssertEqual(viewController.currentMarker?.position.lat, location1.coordinate.latitude)
+        XCTAssertEqual(viewController.currentMarker?.position.lng, location1.coordinate.longitude)
+        
+        let firstMarker = viewController.currentMarker
+        
+        viewController.updateMarker(at: location2)
+        
+        XCTAssertNotNil(viewController.currentMarker)
+        XCTAssertNotEqual(viewController.currentMarker, firstMarker)
+        XCTAssertEqual(viewController.currentMarker?.position.lat, location2.coordinate.latitude)
+        XCTAssertEqual(viewController.currentMarker?.position.lng, location2.coordinate.longitude)
     }
-
 }
